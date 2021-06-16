d9b64f1035247590731cb80f654316dc8f023650
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-16 11:37:52 -0500

    Generación de token Android para el envio de mensajes push

--- push/lib/main.dart
+++ push/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:push/pages/home.dart';
 import 'package:push/pages/message.dart';
+import 'package:push/services/push_services.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); //asegura que por lo menos el context esta listo antes de continuar el resto de instrucciones.
+  await PushServiceCustom.initializeApp();
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
--- push/lib/services/push_services.dart
+++ push/lib/services/push_services.dart
@@ -1,1 +1,20 @@
 //29:1A:8E:1B:7D:E6:91:3C:5E:6B:AC:6E:97:EC:E7:86:C4:5C:37:91
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class PushServiceCustom {
+  static FirebaseMessaging messaginq = FirebaseMessaging
+      .instance; //obtiene la instancia del proyecto e informacion del mismo.
+
+  static String?
+      token; //? quie3re decir opcional y hace referencia al token par ael envio de mensajes.
+
+  static Future initializeApp() async {
+//Push Notifications.
+    await Firebase.initializeApp(); //Inixializa todos los componentes.
+    token = await FirebaseMessaging.instance.getToken(); //Se obtiene el token
+    print('PushServiceCustom - token : $token');
+
+//Local Notifications.
+  }
+}
