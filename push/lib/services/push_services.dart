12f24a66fafc127b15a86ff687b262512e3d8517
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-16 11:55:43 -0500

    Implementaciones de Handlers para los estados de la APP Foreground, Background y Terminated.

--- push/lib/services/push_services.dart
+++ push/lib/services/push_services.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class PushServiceCustom {
-  static FirebaseMessaging messaginq = FirebaseMessaging
+  static FirebaseMessaging messaging = FirebaseMessaging
       .instance; //obtiene la instancia del proyecto e informacion del mismo.
 
   static String?
@@ -15,6 +15,34 @@ class PushServiceCustom {
     token = await FirebaseMessaging.instance.getToken(); //Se obtiene el token
     print('PushServiceCustom - token : $token');
 
+    //Handlers
+    FirebaseMessaging.onBackgroundMessage(
+        _onbackgroundHandler); //Pasa la refenrencia de la función
+    FirebaseMessaging.onMessage
+        .listen(_onMessageHandler); //Cuando la Aplicacion esta abierta
+    FirebaseMessaging.onMessageOpenedApp.listen(_onMessageHandlerOpenApp);
+
 //Local Notifications.
+  }
+
+  //Cuando la aplicación está abierta, pero en segundo plano (minimizada). Esto suele ocurrir cuando el usuario ha presionado el botón "Inicio" en el dispositivo, ha cambiado a otra aplicación a través del conmutador de aplicaciones o tiene la aplicación abierta en una pestaña diferente (web).
+  static Future _onbackgroundHandler(RemoteMessage message) async {
+    print('_onbackgroundHandler - message.messageId   : ${message.messageId}');
+    print(
+        '_onbackgroundHandler - message.messageType : ${message.messageType}');
+  }
+
+  //Cuando la aplicación está abierta, a la vista y en uso.
+  static Future _onMessageHandler(RemoteMessage message) async {
+    print('_onMessageHandler - message.messageId   : ${message.messageId}');
+    print('_onMessageHandler - message.messageType : ${message.messageType}');
+  }
+
+  //Cuando el dispositivo está bloqueado o la aplicación no se está ejecutando. El usuario puede terminar una aplicación "deslizándola hacia afuera" a través de la interfaz de usuario del conmutador de aplicaciones en el dispositivo o cerrando una pestaña (web).
+  static Future _onMessageHandlerOpenApp(RemoteMessage message) async {
+    print(
+        '_onMessageHandlerOpenApp - message.messageId   : ${message.messageId}');
+    print(
+        '_onMessageHandlerOpenApp - message.messageType : ${message.messageType}');
   }
 }
